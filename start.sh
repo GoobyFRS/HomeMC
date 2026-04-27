343157cbd1bf86e4a2bb01e6b99384a669f4b881
Maattt GoobyFRS <68407017+GoobyFRS@users.noreply.github.com>
2026-04-26 17:49:08 -0700

    Update start.sh
    again sad

--- start.sh
+++ start.sh
@@ -23,5 +23,5 @@ exec java \
   -XX:SurvivorRatio=32 \
   -Dusing.aikars.flags=https://mcflags.emc.gs \
   -Daikars.new.flags=true \
-  -jar /opt/HomeMC/prod_server.jar \
+  -jar /home/matt/prod_server.jar \
   --nogui
